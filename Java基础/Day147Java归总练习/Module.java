c0075e73771033f1f25d6189d972256076f932e2
DESKTOP-9K2OTTT\12852 <heyangli51@gmail.com>
2023-07-04 16:00:54 +0900

    no message

---
+++ Java基础/Day147Java归总练习/Module.java
@@ -0,0 +1,9 @@
+package Day0609;
+
+public class Module {
+    public static void main(String[] args) {
+        int student = 26;
+        int leftOut = 26 % 3;
+        System.out.println(leftOut);
+    }
+}
